01877de1d25746ddbf390c286c3b993a85cbd136
Ashley Dennis <adenn1852@gmail.com>
2022-04-01 00:27:42 -0400

    fixed img

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,9 +1,10 @@
+import headshot from "../images/headshot.jpg"
 
 export default function Hero(){
     return (
         <>
         <h1>Ashley Dennis</h1>
-        <img src="./images/headshot.jpg" alt="woman smiling and standing by blue chair" />
+        <img className="headshot" src={headshot} alt="woman smiling and standing by blue chair" />
         </>
     )
 
