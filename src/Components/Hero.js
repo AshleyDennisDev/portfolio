db38580e97a21bb1b51b4c81008b8781dc1f0a54
Ashley Dennis <adenn1852@gmail.com>
2022-03-31 23:58:04 -0400

    first push

---
+++ src/Components/Hero.js
@@ -0,0 +1,10 @@
+
+export default function Hero(){
+    return (
+        <>
+        <h1>Ashley Dennis</h1>
+        <img src="./images/headshot.jpg" alt="woman smiling and standing by blue chair" />
+        </>
+    )
+
+}
